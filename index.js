b8430af1745c38b7e8bdacd0c63ac06679b757f5
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2022-05-24 10:35:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -413,6 +413,7 @@ const feedbackableStages = Object.keys(feedbackableStagesData);
 const permissions = {
   permissionsView: ["kirithiv@navgurukul.org", "vaibhavmagar@navgurukul.org"],
   updateStage: [
+    "sapnapandey20@navgurukul.org",
     "rupakumari20@navgurukul.org",
     "reena@navgurukul.org",
     "tarun20@navgurukul.org",
